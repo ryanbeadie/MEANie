29d7e6f29034c1c866c7e08d0d016fc7530e7a9c
Ryan Beadie <ryansteelebeadie@yahoo.com>
2017-05-10 16:43:07 -0500

    cleanup commit

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,38 @@
 var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
+
+//set up controller
+myApp.controller( 'WhereMyPeeps', function( $http ){
+
+//global to this controller
 var vm = this;
+
+//send record to the server
 vm.addRecord = function(){
-var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
-};
+//created from dom input
+  var objectToSend ={
+  name : vm.nameIn,
+  location : vm.locationIn,
+  };
+console.log(objectToSend);
 $http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
+  method: 'POST',
+  url: '/testPost',
+  data: objectToSend
 });
 vm.nameIn ='';
 vm.locationIn='';
-};
+};//end addRecord
+
+//get records from server/db
 vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
+  console.log('in getRecords function');
+$http({
+  method: 'GET',
+  url: '/getRecords',
 }).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
-}]);
+  console.log('response=' , response);
+vm.allTheRecords = response.data;//changed to .data
+console.log( 'all the records' , vm.allTheRecords );
+    });//end response
+  };//end get records
+});//end controller
--- server/app.js
+++ server/app.js
@@ -1,28 +1,44 @@
+//requires
 var express = require( 'express' );
 var app=express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
-app.use( bodyParser.json() );
 var mongoose = require( 'mongoose' );
+
+//uses
+app.use( express.static( 'public' ) );
+app.use( bodyParser.json() );
+
+
 // 27017 is default mongo port
 mongoose.connect( 'localhost:27017/meanie' );
+
+//schema
 var ourSchema = new  mongoose.Schema({
 name: String,
 location: String
-});
+});//end Schema
+
+//model
 var ourModel = mongoose.model( 'ourModel', ourSchema );
+
 app.get( '/', function( req, res ){
 res.sendFile( path.resolve( 'public/index.html' ) );
 });
+
+
 app.get( '/getRecords', function( req, res ){
 // get and send back all the things
 ourModel.find().then( function( data ){
 res.send( data );
 });
-});
+});//end get
+
 app.listen( 8080, 'localhost', function( req, res ){
 console.log( 'listening on 8080' );
 });
+
+
 app.post( '/testPost', function( req, res ){
 console.log( 'req.body.name: ' + req.body.name );
 // retrieved the req.body
@@ -30,9 +46,8 @@ console.log( 'req.body.name: ' + req.body.name );
 var recordToAdd={
 name:req.body.name,
 location:req.body.location
-}
+};
 // create new record
 var newRecord=ourModel( recordToAdd );
 newRecord.save();
-});
-app.use( express.static( 'public' ) );
+});//end post
